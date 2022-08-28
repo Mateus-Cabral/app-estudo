57070629a59b7c61134420f8fb8b4d0c55d6e7d3
Mateus-Cabral <RM93000@fiap.com.br>
2022-08-28 10:24:50 -0300

    HelloWorld

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,11 @@
+function HelloWorld(){
+    
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h2>Meu primeiro componente</h2>
+        </div>
+    )
+} 
+
+export default HelloWorld
