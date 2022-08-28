36704545df17a80d94433ee09c7fac9bbdf015a3
Mateus-Cabral <RM93000@fiap.com.br>
2022-08-28 10:17:28 -0300

    Configurações iniciais do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import MainRoutes from './routers/MainRoutes';
+import Menu from './components/Menu';
+
 
 function App() {
+  // const name = "Cabral"
+  // const newName = name.toUpperCase()
+
+  // function sum(a,b){
+  //   return a + b
+  // }
+
+  // const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Olá, mundo</h1>
+      <p>Meu primeiro app</p>
+      <p>Olá, {name}</p>
+      <p>Olá, {newName}</p>
+      <img src={url} alt="Minha imagem" />
+      <HelloWorld/>
+      <SayMyName nome="Mateus"/>
+      <Frase/>
+      <Frase/>
+      <Pessoa 
+        nome="Mateus" 
+        idade="25" 
+        profissao="Programador" 
+        foto={url}
+      />
+      <List marca="Ferrari" ano={2010}/>
+      <Frase />
+      <h1>Testando Eventos</h1>
+      <Evento />
+      <Evento numero={1}/>
+      <Evento numero={2}/>
+      <Form /> */}
+      <Menu />
+      <MainRoutes />
+
     </div>
   );
 }
